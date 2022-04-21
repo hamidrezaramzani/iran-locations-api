d256ec4919c80639088a77639ecee964e15891ca
Hamidreza <hrra13801396h@gmail.com>
2022-04-21 14:26:41 +0430

    Change header styles

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,13 +1,37 @@
-import Link from "next/link"
-import { Grid } from '@mui/material'
-import React from 'react'
-
+import { AppBar, Container, Grid, Toolbar, Typography, Button, Box } from '@mui/material'
+import React from 'react';
+import Link from "next/link";
+import LightModeIcon from '@mui/icons-material/LightMode';
 const Header = () => {
   return (
-    <Grid container fontFamily="iran-yekan" xs="12" height="50px" padding="20px" justifyContent="center" gap="10px">
-      <Link href="" >راهنما</Link>
-      <Link href="" >بلاگ</Link>
-      <Link href="" >گیت هاب </Link>
+    <Grid container fontFamily="iran-yekan" xs="12" height="50px" paddingY="20px" justifyContent="center" gap="10px">
+      <Container>
+        <AppBar position="static" color="default" sx={{ boxShadow: "none", borderRadius: "5px" }}>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                <img src="/logo.png" width="70px" />
+              </Typography>
+              <Box sx={{ flexGrow: 1, fontSize: "14px", gap: "15px", display: "flex" }}>
+                <Link href="/">راهنما</Link>
+                <Link href="/">گیت هاب</Link>
+                <Link href="/">درباره من</Link>
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <Button>
+                  <LightModeIcon />
+                </Button>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Container>
     </Grid>
   )
 }
--- components/Header/Welcome/Welcome.js
+++ components/Header/Welcome/Welcome.js
@@ -1,13 +1,13 @@
 import { Grid } from '@mui/material'
 import React from 'react'
-import Example from './Example';
 import Introduction from './Introduction';
-import styles from "./styles.module.css";
 const Welcome = () => {
     return (
         <Grid xs="12" md="10" lg="10" container>
-            <Example />
             <Introduction />
+            <Grid xs="12" md="6" lg="6" borderRadius="10px" padding="10px" >
+                <img src="/vector.png" width="100%" />
+            </Grid>
         </Grid>
     )
 }
