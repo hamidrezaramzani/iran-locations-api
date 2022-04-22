b3efae40cc0875c52b1262b9bec438e6cd3148f2
Hamidreza <hrra13801396h@gmail.com>
2022-04-22 13:06:06 +0430

    Fix cities param to state

--- pages/api/v1/cities.js
+++ pages/api/v1/cities.js
@@ -1,12 +1,12 @@
 const data = require("../../../public/iran_cities_with_coordinates.json");
 export default function handler(req, res) {
-    const { city } = req.query;
-    console.log(city);
-    const state = data.find(state => state.name === city);
-    if (!city || !state)
+    const { state } = req.query;
+    const stateItem = data.find(item => item.name === state);
+    console.log(data);
+    if (!state || !stateItem)
         return res.status(400).json({
-            message: "invalid request - please send  a valid city name with `city` param"
+            message: "invalid request - please send  a valid state name with `state` param"
         })
 
-    res.status(200).json(state.cities);
+    res.status(200).json(stateItem.cities);
 }
