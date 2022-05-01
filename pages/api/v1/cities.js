a08e29c697181de196f84fe44f62b7a60430dd43
Hamidreza <hrra13801396h@gmail.com>
2022-05-01 17:04:38 +0430

    find cities with some letters

--- pages/api/v1/cities.js
+++ pages/api/v1/cities.js
@@ -7,11 +7,11 @@ export default async function handler(req, res) {
 
     cors(req, res);
     const { state } = req.query;
-    const stateItem = data.find(item => item.name === state);
+    const stateItem = data.find(item => item.name.includes(state));
     if (!state || !stateItem)
         return res.status(400).json({
             message: "Send a valid state name with `state` parameter"
         })
 
-    res.status(200).json(stateItem.cities);
+    res.status(200).json(stateItem);
 }
