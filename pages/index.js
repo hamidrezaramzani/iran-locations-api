ca4e97fad65b18dcb3c0506d5e6dc819678d57e6
hamidrezaramzani <hamidrezaramzani80@gmail.com>
2023-04-17 13:48:07 +0330

    add google web master tools config

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import { ThemeContext } from '../context/ThemeProvider';
-import { CssBaseline, Grid, Typography } from '@mui/material'
-import Head from 'next/head'
-import { useContext, useMemo } from 'react';
-import Examples from '../components/Examples/Examples';
-import Guide from '../components/Guide/Guide';
-import Header from '../components/Header/Header';
-import Introduction from '../components/Header/Introduction/Introduction';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeContext } from "../context/ThemeProvider";
+import { CssBaseline, Grid, Typography } from "@mui/material";
+import Head from "next/head";
+import { useContext, useMemo } from "react";
+import Examples from "../components/Examples/Examples";
+import Guide from "../components/Guide/Guide";
+import Header from "../components/Header/Header";
+import Introduction from "../components/Header/Introduction/Introduction";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import styles from "../styles/Home.module.css";
 import Footer from "../components/Footer/Footer";
 export default function Home({ domain }) {
-
   const { state } = useContext(ThemeContext);
   const theme = useMemo(
     () =>
@@ -19,18 +18,25 @@ export default function Home({ domain }) {
           mode: state,
         },
       }),
-    [state],
+    [state]
   );
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Head>
         <title>وب‌سرویس شهر و استان‌های ایران</title>
-        <meta name="description" content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید" />
+        <meta
+          name="description"
+          content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید"
+        />
         <meta name="keywords" content="وب سرویس, شهر, ایران, api" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="author" content="Hamidreza Ramzani" />
+        <meta
+          name="google-site-verification"
+          content="e-lqYCfqL9Ln30jqpJrH0hrJtDm2ytuv1GFXuyuR_dI"
+        />
 
         <link
           rel="preload"
@@ -49,22 +55,42 @@ export default function Home({ domain }) {
         />
         <link rel="icon" href="/logo.png" />
 
-
-        <meta property="og:url" content="https://iran-locations-api.vercel.app/" />
+        <meta
+          property="og:url"
+          content="https://iran-locations-api.vercel.app/"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="وب‌سرویس شهر و استان‌های ایران" />
-        <meta property="og:description" content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید" />
+        <meta
+          property="og:description"
+          content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید"
+        />
         <meta property="og:image" content="" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="iran-locations-api.vercel.app" />
-        <meta property="twitter:url" content="https://iran-locations-api.vercel.app/" />
+        <meta
+          property="twitter:domain"
+          content="iran-locations-api.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://iran-locations-api.vercel.app/"
+        />
         <meta name="twitter:title" content="وب‌سرویس شهر و استان‌های ایران" />
-        <meta name="twitter:description" content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید" />
+        <meta
+          name="twitter:description"
+          content=".وب‌سرویس شهر و استان‌های ایران. با این سرویس می‌توانید به اطلاعات شهر و استان‌های ایران دسترسی پیدا کنید"
+        />
         <meta name="twitter:image" content="" />
-
       </Head>
-      <Grid width="100%" display="flex" justifyContent="center" container height="100vh" className={state === "light" ? styles.darkWelcome : styles.lightWelcome}>
+      <Grid
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        container
+        height="100vh"
+        className={state === "light" ? styles.darkWelcome : styles.lightWelcome}
+      >
         <Header />
         <Introduction />
       </Grid>
@@ -72,9 +98,8 @@ export default function Home({ domain }) {
       <Guide domain={domain} />
       <Footer />
     </ThemeProvider>
-  )
+  );
 }
-
 
 export async function getServerSideProps({ req }) {
   const domain = req.headers.host;
