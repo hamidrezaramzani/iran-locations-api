050ed186ea36662192b477f1d32db83a6e4a081e
Hamidreza <hamidrezaramzani80@gmail.com>
2024-03-01 13:45:40 +0330

    chore: add husky for validations

--- components/Footer/SocialMediaList.js
+++ components/Footer/SocialMediaList.js
@@ -1,34 +1,47 @@
-import { Button, ButtonGroup, Grid } from '@mui/material'
-import React from 'react'
-import Link from 'next/link'
-import TwitterIcon from '@mui/icons-material/Twitter';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { Button, ButtonGroup, Grid } from "@mui/material";
+import React from "react";
+import Link from "next/link";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Image from "next/image";
 const SocialMediaList = () => {
-    return (
-        <Grid display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap="10px">
-            <ButtonGroup variant="text" color="inherit" sx={{ direction: "ltr" }}>
-                <Button>
-                    <Link href="https://twitter.com/iamhamidreza_">
-                        <TwitterIcon />
-                    </Link>
-                </Button>
-                <Button>
-                    <Link href="https://github.com/hamidrezaramzani">
-                        <GitHubIcon />
-                    </Link>
-                </Button>
-                <Button>
-                    <Link href="https://t.me/hamidrezaramzani">
-                        <TelegramIcon />
-                    </Link>
-                </Button>
-            </ButtonGroup>
-            <a href="https://daramet.com/iamhamidreza">
-                <img src="/donation.png" width="160px" />
-            </a>
-        </Grid>
-    )
-}
+  return (
+    <Grid
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      gap="10px"
+    >
+      <ButtonGroup variant="text" color="inherit" sx={{ direction: "ltr" }}>
+        <Button>
+          <Link passHref href="https://twitter.com/iamhamidreza_">
+            <TwitterIcon />
+          </Link>
+        </Button>
+        <Button>
+          <Link passHref href="https://github.com/hamidrezaramzani">
+            <GitHubIcon />
+          </Link>
+        </Button>
+        <Button>
+          <Link passHref href="https://t.me/hamidrezaramzani">
+            <TelegramIcon />
+          </Link>
+        </Button>
+      </ButtonGroup>
+      <a href="https://daramet.com/iamhamidreza">
+        <Image
+          src="/donation.png"
+          alt="donation"
+          width={160}
+          height={30}
+          layout="fixed"
+        />
+      </a>
+    </Grid>
+  );
+};
 
-export default SocialMediaList
+export default SocialMediaList;
