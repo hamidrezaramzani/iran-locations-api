251b7fe57de15fbc9d2a110aa886bafecef5e89d
Hamidreza <hamidrezaramzani80@gmail.com>
2024-05-21 22:46:07 +0330

    feat: add all cities api endpoint in english

--- pages/api/v1/en/cities.js
+++ pages/api/v1/en/cities.js
@@ -1,26 +1,35 @@
-import { cors } from '../../../../middlewares/cors'
+import { cors } from "../../../../middlewares/cors";
 
-const data = require('../../../../public/iran_cities_in_english.json')
+const data = require("../../../../public/iran_cities_in_english.json");
 
-const searchByName = (name) =>
-	data.find((item) =>
-		item.name.toLocaleLowerCase().includes(name.toLowerCase())
-	)
+const searchByName = (name) => data.find((item) => item.name.includes(name));
 
 const searchById = (id) => {
-	return data.find((item) => item.id.toString() === id)
-}
+  return data.find((item) => item.id.toString() === id);
+};
 
 export default async function handler(req, res) {
-	cors(req, res)
-	const { state, state_id } = req.query
-	const stateItem = state ? searchByName(state) : searchById(state_id)
+  cors(req, res);
+  const { state, stateId } = req.query;
+  if (!stateId && !state) {
+    const allCities = [];
+    data.forEach((state, index) => {
+      state.cities.forEach((city) => {
+        allCities.push({
+          ...city,
+          id: allCities.length ? allCities[allCities.length - 1].id + 1 : 1,
+        });
+      });
+    });
 
-	if ((!state && !state_id) || !stateItem) {
-		return res.status(400).json({
-			message: 'Send a valid state name with `state` or `state_id` parameter',
-		})
-	}
+    return res.status(200).json(allCities);
+  }
+  const stateItem = state ? searchByName(state) : searchById(stateId);
+  if ((!state && !stateId) || !stateItem) {
+    return res.status(400).json({
+      message: "Send a valid state name with `state` or `state_id` parameter",
+    });
+  }
 
-	res.status(200).json(stateItem)
+  res.status(200).json(stateItem);
 }
