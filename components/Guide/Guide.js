b9a84d5abd5b23ae84714c7f42691e9333dd860c
Hamidreza <hamidrezaramzani80@gmail.com>
2024-05-21 22:42:57 +0330

    feat: add all cities api endpoint

--- components/Guide/Guide.js
+++ components/Guide/Guide.js
@@ -1,76 +1,85 @@
-import { Container, Grid } from '@mui/material'
-import React from 'react'
-import GuideItem from './GuidItem'
-import Item from './GuidItem'
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import GuideItem from "./GuidItem";
+import Item from "./GuidItem";
 
 const Guide = ({ domain }) => {
-	return (
-		<Grid width='100%' paddingY='50px' id='guide'>
-			<Container>
-				<GuideItem>
-					<GuideItem.Title>زبان داده های ارسالی</GuideItem.Title>
-					<GuideItem.Description>داده ها در فرمت فارسی</GuideItem.Description>
-					<GuideItem.Url domain={domain}>/api/v1/fa/</GuideItem.Url>
-					<GuideItem.Description>داده ها در فرمت انگلیسی</GuideItem.Description>
-					<GuideItem.Url domain={domain}>/api/v1/en/</GuideItem.Url>
-				</GuideItem>
-				<GuideItem>
-					<GuideItem.Title>تمامی استان‌های ایران</GuideItem.Title>
-					<GuideItem.Description>
-						با استفاده از دستور زیر می‌توانید به تمامی استان‌های ایران به‌صورت
-						یک‌جا دسترسی داشته باشید
-					</GuideItem.Description>
-					<GuideItem.Url domain={domain}>/api/v1/fa/states</GuideItem.Url>
-				</GuideItem>
-				<GuideItem>
-					<GuideItem.Title>شهر های یک استان</GuideItem.Title>
-					<GuideItem.Description>
-						شما می‌توانید شهر‌های ایران را با وارد کردن نام یا ایدی استان به
-						عنوان ورودی دریافت کنید
-					</GuideItem.Description>
+  return (
+    <Grid width="100%" paddingY="50px" id="guide">
+      <Container>
+        <GuideItem>
+          <GuideItem.Title>زبان داده های ارسالی</GuideItem.Title>
+          <GuideItem.Description>داده ها در فرمت فارسی</GuideItem.Description>
+          <GuideItem.Url domain={domain}>/api/v1/fa/</GuideItem.Url>
+          <GuideItem.Description>داده ها در فرمت انگلیسی</GuideItem.Description>
+          <GuideItem.Url domain={domain}>/api/v1/en/</GuideItem.Url>
+        </GuideItem>
+        <GuideItem>
+          <GuideItem.Title>تمامی استان‌های ایران</GuideItem.Title>
+          <GuideItem.Description>
+            با استفاده از دستور زیر می‌توانید به تمامی استان‌های ایران به‌صورت
+            یک‌جا دسترسی داشته باشید
+          </GuideItem.Description>
+          <GuideItem.Url domain={domain}>/api/v1/fa/states</GuideItem.Url>
+        </GuideItem>
+        <GuideItem>
+          <GuideItem.Title>شهر های یک استان</GuideItem.Title>
+          <GuideItem.Description>
+            شما می‌توانید شهر‌های ایران را با وارد کردن نام یا ایدی استان به
+            عنوان ورودی دریافت کنید
+          </GuideItem.Description>
 
-					<GuideItem.Inputs>
-						<GuideItem.InputItem>{'{state}'}</GuideItem.InputItem>
-						<GuideItem.InputItem>
-							نام استانی که شهر‌های آن را می‌خواهید
-						</GuideItem.InputItem>
-						<GuideItem.InputItem>بله</GuideItem.InputItem>
-					</GuideItem.Inputs>
-					<GuideItem.Url domain={domain}>
-						/api/v1/fa/cities?state={'{state}'}
-					</GuideItem.Url>
+          <GuideItem.Inputs>
+            <GuideItem.InputItem>{"{state}"}</GuideItem.InputItem>
+            <GuideItem.InputItem>
+              نام استانی که شهر‌های آن را می‌خواهید
+            </GuideItem.InputItem>
+            <GuideItem.InputItem>بله</GuideItem.InputItem>
+          </GuideItem.Inputs>
+          <GuideItem.Url domain={domain}>
+            /api/v1/fa/cities?state={"{state}"}
+          </GuideItem.Url>
 
-					<GuideItem.Inputs>
-						<GuideItem.InputItem>{'{state_id}'}</GuideItem.InputItem>
-						<GuideItem.InputItem>
-							.ایدی استانی که شهر‌های آن را می‌خواهید
-						</GuideItem.InputItem>
-						<GuideItem.InputItem>بله</GuideItem.InputItem>
-					</GuideItem.Inputs>
-					<GuideItem.Url domain={domain}>
-						/api/v1/fa/cities?state_id={'{state_id}'}
-					</GuideItem.Url>
-				</GuideItem>
+          <GuideItem.Inputs>
+            <GuideItem.InputItem>{"{state_id}"}</GuideItem.InputItem>
+            <GuideItem.InputItem>
+              .ایدی استانی که شهر‌های آن را می‌خواهید
+            </GuideItem.InputItem>
+            <GuideItem.InputItem>بله</GuideItem.InputItem>
+          </GuideItem.Inputs>
+          <GuideItem.Url domain={domain}>
+            /api/v1/fa/cities?state_id={"{state_id}"}
+          </GuideItem.Url>
+        </GuideItem>
 
-				<GuideItem>
-					<GuideItem.Title>دریافت استان با آیدی</GuideItem.Title>
-					<GuideItem.Description>
-						هر استانی آیدی منحصر‌به‌فرد خود را دارد و با استفاده از آیدی آن به
-						عنوان ورودی می‌توانید به آن استان دسترسی داشته باشید
-					</GuideItem.Description>
+        <GuideItem>
+          <GuideItem.Title>دریافت استان با آیدی</GuideItem.Title>
+          <GuideItem.Description>
+            هر استانی آیدی منحصر‌به‌فرد خود را دارد و با استفاده از آیدی آن به
+            عنوان ورودی می‌توانید به آن استان دسترسی داشته باشید
+          </GuideItem.Description>
 
-					<GuideItem.Inputs>
-						<GuideItem.InputItem>{'{id}'}</GuideItem.InputItem>
-						<GuideItem.InputItem>آیدی استان</GuideItem.InputItem>
-						<GuideItem.InputItem>بله</GuideItem.InputItem>
-					</GuideItem.Inputs>
-					<GuideItem.Url domain={domain}>
-						/api/v1/fa/states?id={'{id}'}
-					</GuideItem.Url>
-				</GuideItem>
-			</Container>
-		</Grid>
-	)
-}
+          <GuideItem.Inputs>
+            <GuideItem.InputItem>{"{id}"}</GuideItem.InputItem>
+            <GuideItem.InputItem>آیدی استان</GuideItem.InputItem>
+            <GuideItem.InputItem>بله</GuideItem.InputItem>
+          </GuideItem.Inputs>
+          <GuideItem.Url domain={domain}>
+            /api/v1/fa/states?id={"{id}"}
+          </GuideItem.Url>
+        </GuideItem>
 
-export default Guide
+        <GuideItem>
+          <GuideItem.Title>دریافت تمامی شهر های ایران</GuideItem.Title>
+          <GuideItem.Description>
+            با استفاده از دستور زیر میتوانید تمامی شهر های ایران را یک جا داشته
+            باشید
+          </GuideItem.Description>
+          <GuideItem.Url domain={domain}>/api/v1/fa/cities</GuideItem.Url>
+        </GuideItem>
+      </Container>
+    </Grid>
+  );
+};
+
+export default Guide;
--- pages/api/v1/fa/cities.js
+++ pages/api/v1/fa/cities.js
@@ -1,22 +1,35 @@
-import { cors } from '../../../../middlewares/cors'
+import { cors } from "../../../../middlewares/cors";
 
-const data = require('../../../../public/iran_cities_with_coordinates.json')
+const data = require("../../../../public/iran_cities_with_coordinates.json");
 
-const searchByName = (name) => data.find((item) => item.name.includes(name))
+const searchByName = (name) => data.find((item) => item.name.includes(name));
 
 const searchById = (id) => {
-	return data.find((item) => item.id.toString() === id)
-}
+  return data.find((item) => item.id.toString() === id);
+};
 
 export default async function handler(req, res) {
-	cors(req, res)
-	const { state, state_id } = req.query
-	const stateItem = state ? searchByName(state) : searchById(state_id)
-	if ((!state && !state_id) || !stateItem) {
-		return res.status(400).json({
-			message: 'Send a valid state name with `state` or `state_id` parameter',
-		})
-	}
+  cors(req, res);
+  const { state, stateId } = req.query;
+  if (!stateId && !state) {
+    const allCities = [];
+    data.forEach((state, index) => {
+      state.cities.forEach((city) => {
+        allCities.push({
+          ...city,
+          id: allCities.length ? allCities[allCities.length - 1].id + 1 : 1,
+        });
+      });
+    });
 
-	res.status(200).json(stateItem)
+    return res.status(200).json(allCities);
+  }
+  const stateItem = state ? searchByName(state) : searchById(stateId);
+  if ((!state && !stateId) || !stateItem) {
+    return res.status(400).json({
+      message: "Send a valid state name with `state` or `state_id` parameter",
+    });
+  }
+
+  res.status(200).json(stateItem);
 }
