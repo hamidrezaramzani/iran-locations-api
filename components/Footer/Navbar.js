8d90d7166bed485ad44b2cf165c8b990ec1960fd
Hamidreza <hamidrezaramzani80@gmail.com>
2024-05-23 15:36:05 +0330

    feat: add notification telegram channle

--- components/Footer/Navbar.js
+++ components/Footer/Navbar.js
@@ -1,22 +1,28 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import Link from 'next/link'
+import { Grid } from "@mui/material";
+import React from "react";
+import Link from "next/link";
 const Navbar = () => {
-    return (
-        <Grid display="flex" justifyContent="center" paddingY="10px" fontSize="13px" fontFamily="iran-yekan" color="#888" gap="10px">
-            <Link href="https://github.com/hamidrezaramzani">
-               حمیدرضا رمضانی
-            </Link>
+  return (
+    <Grid
+      display="flex"
+      justifyContent="center"
+      paddingY="10px"
+      fontSize="13px"
+      fontFamily="iran-yekan"
+      color="#888"
+      gap="10px"
+    >
+      <Link href="https://github.com/hamidrezaramzani">حمیدرضا رمضانی</Link>
 
-            <Link href="/#guide">
-                راهنمایی
-            </Link>
+      <Link href="/#guide">راهنمایی</Link>
 
-            <Link href="https://github.com/pesarkhobeee/iran-states-and-cities-json-and-sql-including-area-coordinations.git">
-               مخزن داده ها
-            </Link>
-        </Grid>
-    )
-}
+      <Link href="https://github.com/pesarkhobeee/iran-states-and-cities-json-and-sql-including-area-coordinations.git">
+        مخزن داده ها
+      </Link>
 
-export default Navbar
+      <Link href="https://t.me/iranlocationsapi">کانال اطلاع رسانی</Link>
+    </Grid>
+  );
+};
+
+export default Navbar;
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,20 +1,39 @@
-import { AppBar, Container, Grid, Toolbar, Typography, Button, Box } from '@mui/material'
-import React, { useContext } from 'react';
+import {
+  AppBar,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
+import React, { useContext } from "react";
 import Link from "next/link";
 import { ThemeContext } from "../../context/ThemeProvider";
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 const Header = () => {
-
   const { state, setState } = useContext(ThemeContext);
 
   const handleClickToggleTheme = () => {
     setState(state === "dark" ? "light" : "dark");
-  }
+  };
   return (
-    <Grid container fontFamily="iran-yekan" xs="12" height="50px" paddingY="20px" justifyContent="center" gap="10px">
+    <Grid
+      container
+      fontFamily="iran-yekan"
+      xs="12"
+      height="50px"
+      paddingY="20px"
+      justifyContent="center"
+      gap="10px"
+    >
       <Container>
-        <AppBar position="static" color="default" sx={{ boxShadow: "none", borderRadius: "5px" }}>
+        <AppBar
+          position="static"
+          color="default"
+          sx={{ boxShadow: "none", borderRadius: "5px" }}
+        >
           <Container maxWidth="xl">
             <Toolbar disableGutters>
               <Typography
@@ -25,10 +44,24 @@ const Header = () => {
               >
                 <img src="/logo.png" width="70px" alt="website logo" />
               </Typography>
-              <Box sx={{ flexGrow: 1, fontSize: "14px", gap: "15px", display: "flex" }}>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  fontSize: "14px",
+                  gap: "15px",
+                  display: "flex",
+                }}
+              >
                 <Link href="/#guide">راهنما</Link>
-                <Link href="https://github.com/hamidrezaramzani/iran-locations-api">گیت هاب</Link>
-                <Link href="https://github.com/hamidrezaramzani/hamidrezaramzani">درباره من</Link>
+                <Link href="https://github.com/hamidrezaramzani/iran-locations-api">
+                  گیت هاب
+                </Link>
+                <Link href="https://github.com/hamidrezaramzani/hamidrezaramzani">
+                  درباره من
+                </Link>
+                <Link href="https://t.me/iranlocationsapi">
+                  کانال اطلاع رسانی
+                </Link>
               </Box>
 
               <Box sx={{ flexGrow: 0 }}>
@@ -41,7 +74,7 @@ const Header = () => {
         </AppBar>
       </Container>
     </Grid>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
