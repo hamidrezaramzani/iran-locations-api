15e565e35cb5e8a4137d8f5417ee187d07e33c0c
Amirreza Rahmani <amirbabble@gmail.com>
2025-03-01 16:26:56 +0330

    Add landline Prefix parameter to APIs

--- pages/api/v1/fa/states.js
+++ pages/api/v1/fa/states.js
@@ -5,10 +5,10 @@ const states = require('../../../../public/iran_cities_with_coordinates.json');
 
 export default async function handler(req, res) {
   cors(req, res);
-  const { id, state } = req.query;
+  const { id, state , landlinePrefix } = req.query;
 
   try {
-    const matchedStates = getStates(states, id, state);
+    const matchedStates = getStates(states, id, state , landlinePrefix);
     return res.status(200).json(matchedStates);
   } catch (error) {
     return res.status(400).json({
